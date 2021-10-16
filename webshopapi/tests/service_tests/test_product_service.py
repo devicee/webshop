64eb187a4f94035b9809985e6050ca2158d27ff9
Refik Hadzialic <refikh@gmail.com>
2021-10-16 15:32:19 +0200

    add unit tests for service layer

---
+++ webshopapi/tests/service_tests/test_product_service.py
@@ -0,0 +1,37 @@
+from django.http import Http404
+from django.test import TestCase
+
+from webshopapi.models import ProductModel
+from webshopapi.service.product_service import ProductService
+
+
+class TestProductService(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        ProductModel.objects.create(name="Lego bricks", price=5.55)
+        ProductModel.objects.create(name="Barbie doll", price=4)
+        ProductModel.objects.create(name="Puzzles", price=10)
+
+    def test__list_products__success(self):
+        products = ProductService.list_products()
+        self.assertEqual(len(products), 3)
+        self.assertEqual(products[0].name, "Lego bricks")
+        self.assertEqual(products[1].name, "Barbie doll")
+        self.assertEqual(products[2].name, "Puzzles")
+
+    def test__get_product_by_id__success(self):
+        product = ProductService.get(pk=1)
+        self.assertEqual(product.name, "Lego bricks")
+        self.assertEqual(float(product.price), 5.55)
+
+    def test__get_product__non_existing_id__fail(self):
+        with self.assertRaises(Http404):
+            __ = ProductService.get(pk=1234)
+
+    def test__post_product__success(self):
+        test_product = {"name": "Dog toy", "price": 34}
+        product = ProductService.post(request=test_product)
+        self.assertNotEqual(product.pk, None)
+
+        product_result = ProductModel.objects.filter(pk=product.pk).first()
+        self.assertEqual(product, product_result)
